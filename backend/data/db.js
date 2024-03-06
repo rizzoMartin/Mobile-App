37b71e3e8af60b52a1e899d3708e0ae4fb6605d8
rizzoMartin <rizzolozanomartin@gmail.com>
2024-03-06 10:07:59 +0100

    cambios en la versión de mysql para compatibilidad con mysql workbench y cambios en el dockerfile del front para mejores prácticas

--- backend/data/db.js
+++ backend/data/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('prueba', 'user', 'user', {
   host: 'database', // Nombre del servicio en docker-compose
   dialect: 'mysql',
-  logging: false, // Puedes activar el logging si quieres ver las consultas SQL en la consola
+  logging: false // Puedes activar el logging si quieres ver las consultas SQL en la consola
 });
 
 module.exports = sequelize;
